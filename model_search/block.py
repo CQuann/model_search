5630ebaae6d751c15f89c7869b461e6560f29455
C_Quan <p9083718@gmail.com>
2026-02-14 12:06:34 +0300

    second commit to branch2

--- model_search/block.py
+++ model_search/block.py
@@ -17,6 +17,7 @@
 # No need to call init of superclass "Block", since Block is an abstract class.
 """Well known building blocks for Convolutional networks."""
 
+import math
 import abc
 
 import enum
