cb24c1060a0f8124c9e8dde0bfb9bb4946b26dee
C_Quan <p9083718@gmail.com>
2026-02-14 12:07:08 +0300

    third commit to branch2

--- model_search/constants.py
+++ model_search/constants.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 """Useful constants."""
 
+SOME_CONSTANT = 12341234
 DEFAULT_DNN = "model_search/model_search/configs/dnn_config.pbtxt"
 DEFAULT_CNN = "model_search/model_search/configs/cnn_config.pbtxt"
 DEFAULT_RNN_ALL = (
